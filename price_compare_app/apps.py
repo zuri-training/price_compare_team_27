d2c3177beb0a67280f116f9988bcb448031443f4
abdullahiola <abdullahi.busari101@gmail.com>
2022-07-26 10:18:00 +0100

    added django files

---
+++ price_compare_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PriceCompareAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'price_compare_app'
---
+++ price_compare_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home_page():
+    pass
+
+def sign_up():
+    pass
+
+
+def login_page():
+    pass
+
+def logout():
+    pass
+
+def search():
+    pass
+
