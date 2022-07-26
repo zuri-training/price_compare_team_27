ae030956bdaf14f3f11b0f0468d95f9e5ef91788
abdullahiola <abdullahi.busari101@gmail.com>
2022-07-26 11:36:53 +0100

    added accounts app

--- price_compare/settings.py
+++ price_compare/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'price_compare_app',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'price_compare.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Lagos'
 
 USE_I18N = True
 
@@ -118,13 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'static'
+
 STATICFILES_DIRS= [
     os.path.join(BASE_DIR,'static')
 ]
 
-MEDIA_ROOT= BASE_DIR/'media'
-MEDIA_URL = '/media/'
 
 
 # Default primary key field type
