9b4b630cf8b190edd9bfa32e635e135de215c744
abdullahiola <abdullahi.busari101@gmail.com>
2022-07-27 01:51:10 +0100

    changed the file structure, fixed login,logout and signin page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
+from .forms import CreateUserForm
+
 
 # Create your views here.
+def sign_up(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form=CreateUserForm()
+        if request.method == "POST":
+            form=CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user=form.cleaned_data.get('username')
+                messages.success(request, "Account was created for " + user)
+
+                return redirect('login')
+
+        context={'form':form}
+        return render(request,'accounts/signup.html',context)
+
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method =="POST":
+            user_name= request.POST.get('username')
+            password= request.POST.get('password')
+
+            user=authenticate(request,username=user_name,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('index')
+            else:
+                messages.info(request,"Username Or Password Is Incorrect")
+
+    context = {}
+    
+    return render(request,'accounts/login.html',context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
--- price_compare_app/urls.py
+++ price_compare_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home_page,name='home')
+    path('',views.home_page,name='index')
 ]
