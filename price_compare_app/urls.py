a772384c7c857a2b9329464bc53d87aa9ca036d4
abdullahiola <abdullahi.busari101@gmail.com>
2022-08-03 09:52:20 +0100

    updated yo seyi branch

--- price_compare_app/urls.py
+++ price_compare_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home_page,name='index'),
-    path('about',views.about_page,name='about')
+    path('about',views.about_page,name='about'),
+
 ]
