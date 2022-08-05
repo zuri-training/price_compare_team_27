d57bfa78927635bf9907668226819c62ddabc270
abdullahiola <abdullahi.busari101@gmail.com>
2022-08-05 18:40:51 +0100

    commit develop

--- price_compare/settings.py
+++ price_compare/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'price_compare.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES ={
-    'default': dj_database_url.config()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES ={
+#     'default': dj_database_url.config()
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 STATICFILES_DIRS= [
     os.path.join(BASE_DIR,'static')
