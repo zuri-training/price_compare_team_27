80ab2e23c5d12c5c2a6ada461f04be460ef630f1
almondb262 <105273749+almondb262@users.noreply.github.com>
2022-08-05 23:43:25 +0100

    Update urls.py

--- price_compare_app/urls.py
+++ price_compare_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('',views.home_page,name='index'),
     path('about',views.about_page,name='about'),
     path('phone/<slug:slug>/', views.PhoneDetailView, name='phone'),
+    path('search',views.search,name='search'),
 
 ]
