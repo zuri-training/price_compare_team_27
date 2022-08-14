c9c44fc838088d5ebf8781221b8e663f1485386c
johnafariogun <afariogun.john@yahoo.com>
2022-08-14 17:03:57 +0100

    deleted multiple wishlist button and removed some console.log statements from the javascript

--- price_compare_app/static/price_compare_app/js/wishlist.js
+++ price_compare_app/static/price_compare_app/js/wishlist.js
@@ -4,8 +4,7 @@ for (i =0; i < updatebtns.length; i++){
     updatebtns[i].addEventListener('click', function(){
         var productId = this.dataset.product
         var action = this.dataset.action
-        console.log('productId:', productId, 'Action:', action)
-        console.log('USER:', user) 
+         
         if (user === 'AnonymousUser'){
             console.log('User is not logged in')}else(
                 updateUserOrder(productId, action)
@@ -15,7 +14,6 @@ for (i =0; i < updatebtns.length; i++){
 }
 
 function updateUserOrder(productId, action){
-    console.log('User is logged in, sending data...')
     var url = '/update_item/'
     fetch(url, {
         method:'POST',
