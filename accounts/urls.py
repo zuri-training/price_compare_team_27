ff2d9e8318c6c0852e08cefd8b82fa6ae6e9549f
almondb262 <austinemma60@gmail.com>
2022-08-16 00:16:19 +0100

    additional changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('signup/',views.sign_up,name='signup'),
     path('logout/ ',views.logout_user,name='logout'),
     path('password-reset/',views.password_reset_request,name='password_reset'),
-    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),name='password_reset_confirm'),
-    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),name='password_reset_complete'),
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),name='password_reset_confirm'),
+    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),name='password_reset_complete'),
     path('newsletter/',views.newsletter,name='newsletter')
 ]
